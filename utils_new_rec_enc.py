1db46f843232eb2f6fba017bec1e86e918a8fb9b
Yiorgos Kalantzis <gk18@sanger.ac.uk>
2024-11-18 16:07:16 +0000

    Update with latest changes from GEL's RE

--- utils_new_rec_enc.py
+++ utils_new_rec_enc.py
@@ -1,5 +1,28 @@
 import numpy as np
 from scipy.stats import beta
+    
+def write_saige_file(df, out_file, col_snp='SNP', col_gene='Gene', 
+                     col_consq='Consq', col_weight='Weight'):
+    # write the SAIGE group file to disk
+    print(f"Saving the annotation to {out_file}")
+    with open(out_file, 'w') as fout:
+        for gene_id, variants in df.groupby(col_gene):
+            annotated = ~variants[col_consq].isna()
+            
+            snps = ' '.join(
+                [snp for snp in variants[col_snp][annotated].values]
+            )
+            annos = ' '.join(
+                [anno for anno in variants[col_consq][annotated].values]
+            )
+            weights = ' '.join(
+                [f'{w:.4f}' for w in variants[col_weight][annotated].values]
+            )
+            
+            fout.write(f"{gene_id} var {snps}\n")
+            fout.write(f"{gene_id} anno {annos}\n")
+            fout.write(f"{gene_id} weight {weights}\n")
+
 
 # ranking of consequences for pairs of variants (less is worse)
 rank_consq = {
@@ -56,7 +79,6 @@ def get_freq_product( markers, df_maf ):
         return 2 * a * b
     else:
         return df_maf.loc[markers].MAF ** 2
-    
 
 new_labels = {
     'pLoF': 'pLoF',
@@ -83,19 +105,20 @@ new_labels = {
 }
 
 new_weights = {
-    'synonymous|synonymous': 0.05,
-    'synonymous': 0.05,
-    'other_missense': 0.025,
-    'other_missense|other_missense': 0.025,
-    'other_missense|damaging_missense': 0.025,
-    'damaging_missense|damaging_missense': 0.025,
-    'damaging_missense|damaging_missense': 0.01,
-    'damaging_missense': 0.01,
-    'other_missense|pLoF': 0.01,
-    'pLoF': 0.001,
-    'pLoF|damaging_missense': 0.001,
-    'pLoF|pLoF': 0.001,
-    'damaging_missense|pLoF': 0.001
+    'synonymous|synonymous': 1,
+    'synonymous': 1,
+    'other_missense': 2,
+    'other_missense|other_missense': 2,
+    'other_missense|damaging_missense': 2,
+    'damaging_missense|other_missense': 2,
+    'damaging_missense|damaging_missense': 3,
+    'other_missense|pLoF': 3,
+    'pLoF|other_missense': 3,
+    'damaging_missense': 4,
+    'pLoF|damaging_missense': 5,
+    'damaging_missense|pLoF': 5,
+    'pLoF|pLoF': 5,
+    'pLoF': 5,
 }
 
 def annot_relabelling(x):
@@ -105,34 +128,14 @@ def annot_relabelling(x):
 # to be used like df['weights'] = df.worst_consq.apply(lambda x: set_weights(x))
 
 def set_class_weights(x, new_weights=new_weights):
-    return beta.pdf(new_weights[x], 1, 25)
-
-def set_random_weights(x):
-    return beta.pdf(np.random.rand()/20, 1, 25)
-
+    # return beta.pdf(new_weights[x], 1, 25)
+    return new_weights[x]
+    
 def set_uniform_weights(x):
     return beta.pdf(1, 1, 25)
 
 def set_af_weights(x):
     return beta.pdf(x, 1, 25)
-    
-def write_saige_file(df, out_file, col_snp='SNP', col_gene='Gene', col_consq='Consq', col_weight='weight'):
-    # write the SAIGE group file to disk
-    print(f"Saving the annotation to {out_file}")
-    with open(out_file, 'w') as fout:
-        for gene_id, variants in df.groupby(col_gene):
-            annotated = ~variants[col_consq].isna()
-            
-            snps = ' '.join(
-                [snp for snp in variants[col_snp][annotated].values]
-            )
-            annos = ' '.join(
-                [anno for anno in variants[col_consq][annotated].values]
-            )
-            weights = ' '.join(
-                [f'{w:.4f}' for w in variants[col_weight][annotated].values]
-            )
-            
-            fout.write(f"{gene_id} var {snps}\n")
-            fout.write(f"{gene_id} anno {annos}\n")
-            fout.write(f"{gene_id} weight {weights}\n")
+
+def set_random_weights(x):
+    return beta.pdf(np.random.rand()/20, 1, 25)
